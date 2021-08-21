177ff9643863df2125c10d9b60a9cdb58f1c1859
gearless_june <taha.akbarally@gmail.com>
2021-08-22 07:24:49 +1000

    commit before changing exits

---
+++ Assets/Scripts/Character/Controllers/Traps/Spinner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : Trap {
+
+    /* --- Constructor --- */
+    public Spinner() {
+        id = 2;
+    }
+
+    public float rotationSpeed;
+    public int damage;
+    public Transform arms;
+
+    protected override void On() {
+        float deltaRotation = rotationSpeed * Time.fixedDeltaTime;
+        arms.eulerAngles = arms.eulerAngles + Vector3.forward * deltaRotation;
+    }
+
+    /* --- Event Actions --- */
+    protected override void OnHit(Hurtbox hurtbox) {
+        hurtbox.controller.Hurt(damage);
+    }
+
+}
---
+++ Assets/Scripts/Character/Meshes/Particle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : Mesh
+{
+    /* --- Components --- */
+    public Sprite[] effect;
+
+    /* --- Variables --- */
+    SpriteRenderer spriteRenderer;
+    int frameRate = 8;
+    float timeInterval = 0f;
+
+    /* --- Unity --- */
+    // Runs once before the first frame.
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sortingLayerName = GameRules.midGround;
+    }
+
+    /* --- Override --- */
+    // The parameters to be rendered every frame
+    public override void Render() {
+        RenderSprite();
+    }
+
+    /* --- Parameters --- */
+    // Renders the sprite based on the state.
+    void RenderSprite() {
+        timeInterval += Time.deltaTime;
+        int index = ((int)Mathf.Floor(timeInterval * frameRate) % effect.Length); ;
+        spriteRenderer.sprite = effect[index];
+    }
+}
